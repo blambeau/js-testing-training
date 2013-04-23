312ab6564e039cd696d5217f7fdaeec74c80fe93
Julien Biezemans <jb@jbpros.com>
2013-04-23 15:56:42 +0200

    Enhancement: Hitting and missing ships
    Here we add the acceptance test for the new behaviour.
    
    Notice we're changing the wording in the test to use more domain
    specific language, talking about shooting, hitting and missing
    instead of clicking. This reflects our growing understanding of the domain.

--- public/scripts/tests.js
+++ public/scripts/tests.js
@@ -1,12 +1,26 @@
 // End-to-end tests
 
-describe("clicking a cell", function () {
-  var $td = $($("td").get(1));
+describe("shooting", function () {
+  describe("when I miss", function () {
+    var $td = $($("td").get(0));
 
-  it("is marked as hit", function() {
-    expect($td.hasClass("hit")).toBeFalsy();
-    $td.click();
-    expect($td.hasClass("hit")).toBeTruthy();
+    it("marks the cell as a miss", function() {
+      expect($td.hasClass("miss")).toBeFalsy();
+      $td.click();
+      expect($td.hasClass("miss")).toBeTruthy();
+    });
+  });
+
+  describe("when I hit a ship", function () {
+    var td = $("td").get(1);
+    var $td = $(td);
+    occupiedCells.push(td);
+
+    it("marks the cell as a hit", function() {
+      expect($td.hasClass("hit")).toBeFalsy();
+      $td.click();
+      expect($td.hasClass("hit")).toBeTruthy();
+    });
   });
 });
 
