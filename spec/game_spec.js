24aba31a02298a430faca1fb5c0fea815bd4ab83
Julien Biezemans <jb@jbpros.com>
2013-04-27 23:36:45 +0200

    Refactor: DRY model spec code
    The model spec contained some repetition: some spies were created
    repeatedly. We removed that repetition by using the beforeEach
    so-called "hook" function provided by Jasmine.
    
    More importantly, we completely decoupled examples from each other by
    forcing new blank instances of our model to be created within them.
    This suppresses all risks of cascading failing tests.
    
    What we learned:
    * Hooks in Jasmine
    * "DRY" principle (and how to not abuse it in tests)
    * "Independant tests" principle: don't share state between tests

--- spec/game_spec.js
+++ spec/game_spec.js
@@ -1,27 +1,28 @@
 var Game = require("../public/scripts/battleship").Game;
 
 describe(Game, function () {
-  var game = new Game();
+  var game, onHit, onMiss;
+
+  beforeEach(function () {
+    game = new Game();
+    onHit = jasmine.createSpy("onHit callback");
+    onMiss = jasmine.createSpy("onMiss callback");
+  });
 
   describe("shootAt", function () {
 
-    describe("when the targetted cell is occupied", function () {
+    describe("when the targeted cell is occupied", function () {
       it ("calls the hit callback with the cell", function () {
         var occupiedCell = "occupied cell";
         game.occupiedCells.push(occupiedCell);
-        var onHit = jasmine.createSpy("onHit callback");
-        var onMiss = jasmine.createSpy("onMiss callback");
         game.shootAt(occupiedCell, onHit, onMiss);
         expect(onHit).toHaveBeenCalledWith(occupiedCell);
       });
     });
 
-    describe("when the targetted cell is free", function () {
+    describe("when the targeted cell is free", function () {
       it("calls the miss callback with the cell", function () {
         var freeCell = "free cell";
-        game.occupiedCells.length = 0;
-        var onHit = jasmine.createSpy("onHit callback");
-        var onMiss = jasmine.createSpy("onMiss callback");
         game.shootAt(freeCell, onHit, onMiss);
         expect(onMiss).toHaveBeenCalledWith(freeCell);
       });
